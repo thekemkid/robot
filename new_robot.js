a6aad16f0eca35621bfc608737ae850af2e592e6
Darragh Hayes <dara.hayes.1995@gmail.com>
2015-09-03 16:35:56 +0100

    machines receiving jobs

--- new_robot.js
+++ new_robot.js
@@ -4,34 +4,52 @@ var raspi = new Raspi()
 var board = new five.Board({
   io: raspi
 })
-var num = 1
-
-var ports = [
-  'GPIO18',
-  'GPIO27',
-  'GPIO22',
-  'GPIO23',
-  'GPIO24',
-  'GPIO9'
-]
+var worker = 'pi1'
+var mqtt;
 
 var machines = [
   {
+    id: 1,
     ports: ['GPIO24', 'GPIO9', 'GPIO18'],
-    ready: false,
+    ready: true,
     pins: {}
   },
   {
+    id: 2,
     ports : ['GPIO22', 'GPIO23', 'GPIO27'],
-    ready: false,
+    ready: true,
     pins: {}
   }
 ];
 
-var mqtt = require('mqtt').connect('mqtt://test.mosca.io')
-
 board.on('ready', function () {
 
+  mqtt = require('mqtt').connect('mqtt://192.168.1.83:2048');
+  initMachines();
+  mqtt.subscribe('pi1');
+
+  this.repl.inject({
+    machines: machines
+  });
+
+  mqtt.publish(worker, JSON.stringify({worker: worker, status: 'ready'}));
+
+  mqtt.on('message', function(topic, message) {
+    console.log(message.toString());
+    message = JSON.parse(message.toString());
+    if (message.jobs) {
+      console.log('received jobs' + JSON.stringify(message.jobs, null, 2));
+      //if both machines are ready to accept jobs
+      if (machines.every(function(machine) { return machine.ready })) {
+        message.jobs.forEach(function(job) {
+          machines[job.pump].runJob(job)
+        });
+      }
+    }
+  });
+})
+
+function initMachines() {
   machines.forEach(function(machine) {
     machine.pins = machine.ports.reduce(function (acc, port) {
       var pin = new five.Pin(port)
@@ -40,39 +58,37 @@ board.on('ready', function () {
     }, {})
 
     machine.reset = function () {
+      var machine = this;
       this.ports.forEach(function (port) {
-        var pin = this.pins[port]
+        var pin = machine.pins[port];
         pin.low()
       })
-
-      mqtt.publish('cocktail/' + num + '/status', 'online')
     }
   
     machine.start = function () {
+      var machine = this;
       this.ports.forEach(function (port) {
-        var pin = this.pins[port]
+        var pin = machine.pins[port]
         pin.high()
       })
     }
-  });
 
-  reset()
+    machine.runJob = function(job) {
+      var machine = this;
+      this.ready = false;
+      console.log('running job on machine', machine.id)
+      mqtt.publish(worker, JSON.stringify({status: 'run job on machine ' + machine.id}));
+      
 
-  this.repl.inject(machines);
+      machine.ports.forEach(function(port, index) {
+        var pin = machine.pins[port];
+        pin.high();
 
-  function reset () {
-    machines.forEach(function (machine) {
-      machine.reset();
-    });
-
-    mqtt.publish('cocktail/' + num + '/status', 'online')
-  }
-
-  function startAll () {
-    machines.forEach(function (machine) {
-      machine.start();
-    })
-
-    setTimeout(reset, 60000)
-  }
-})
+        setTimeout(function() {
+          pin.low();
+          this.ready = true;
+        }, job.activations[index])
+      })
+    }
+  })
+}
---
+++ test_job.js
@@ -0,0 +1,54 @@
+var mqtt = require('mqtt').connect('mqtt://localhost:2048');
+
+var cocktails = {
+  bloody: {
+    activations: [
+      2000, // milleseconds
+      1000,
+      5000
+    ]
+  },
+  spritz: {
+    activations: [
+      4000, // milleseconds
+      8000,
+      12000
+    ]
+  }
+}
+var defs = {
+  cocktails: cocktails,
+  workers: {
+    bob: {
+      cocktails: ['bloody', 'spritz']
+    },
+    mark: {
+      cocktails: ['spritz', 'spritz']
+    }
+  }
+}
+
+var jobs = [{
+        id: 0,
+        name: 'Matteo',
+        cocktail: 'spritz',
+        pump: 1,
+        activations: defs.cocktails.spritz.activations
+      }, {
+        id: 3,
+        name: 'David',
+        cocktail: 'bloody',
+        pump: 0,
+        activations: defs.cocktails.bloody.activations
+      }]
+
+
+mqtt.on('connect', function() {
+	mqtt.subscribe('pi1');
+	mqtt.publish('pi1', JSON.stringify({jobs: jobs}));
+	console.log('pushed messaged');
+})
+
+mqtt.on('message', function(topic, message) {
+	console.log('received message', message.toString());
+})
