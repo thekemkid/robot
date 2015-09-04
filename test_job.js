4d1aeec62169d43b0f26d29f9e4f8e660a915358
Glen Keane <glenkeane.94@gmail.com>
2015-09-04 22:44:55 +0100

    updated recipe list

--- test_job.js
+++ test_job.js
@@ -7,11 +7,25 @@ var EE = require('events').EventEmitter;
 var mqttToSocketIoEE = new EE();
 
 var cocktails = {
+  darkAndStormy: {
+    activations: [
+      {drink: 'ginger beer', time: 120000}, // milleseconds
+      {drink: 'rum', time: 40000},
+      {drink: 'lime', time: 20000}
+    ]
+  },
   rumAndCoke: {
     activations: [
       {drink: 'coke', time: 60000}, // milleseconds
       {drink: 'coke', time: 60000},
       {drink: 'rum', time: 20000}
+    ]
+  },
+  moscowMule: {
+    activations: [
+      {drink: 'vodka', time: 60000}, // milleseconds
+      {drink: 'lime', time: 15000},
+      {drink: 'ginger beer', time: 120000}
     ]
   },
   seabreeze: {
@@ -21,6 +35,20 @@ var cocktails = {
       {drink: 'pineapple', time: 120000}
     ]
   },
+  whiskeySour: {
+    activations: [
+      {drink: 'whiskey', time: 60000}, // milleseconds
+      {drink: 'lemon juice', time: 30000},
+      {drink: 'gomme syrup', time: 15000}
+    ]
+  },
+  crazyWolf: {
+    activations: [
+      {drink: 'whiskey', time: 60000}, // milleseconds
+      {drink: 'peach schnapps', time: 30000},
+      {drink: 'pinapple', time: 120000}
+    ]
+  }
   crazy: {
     activations: [
       {drink: 'whiskey', time: 60000}, // milleseconds
